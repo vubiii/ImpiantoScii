2406967ecf8c09009345e00ba802f63d328328a0
Greta Savio <g.savio@itsrizzoli.it>
2024-01-15 17:20:48 +0100

    piste aperte - se segmento chiuso tutto chiuso

--- main.py
+++ main.py
@@ -8,41 +8,70 @@ class NeoManager:
         self.neo4j_password = neo4j_password
         self.driver = None
 
-    def open(self):
-        print(f"Collegandosi al database {self.neo4j_uri}.")
-        self.driver = GraphDatabase.driver(
-            self.neo4j_uri,
-            auth=(self.neo4j_username, self.neo4j_password))
-        print(f"Collegato al database {self.neo4j_uri}.")
+    def apri_connessione(self):
+        print(f"Connessione al database {self.neo4j_uri}.")
+        try:
+            self.driver = GraphDatabase.driver(
+                self.neo4j_uri,
+                auth=(self.neo4j_username, self.neo4j_password))
+            print(f"Connessione al database {self.neo4j_uri} stabilita.")
+        except Exception as e:
+            print(f"Errore durante la connessione: {e}")
+            traceback.print_exc()
 
-    def close(self):
+    def chiudi_connessione(self):
         print("Chiusura della connessione al database.")
         if self.driver:
             self.driver.close()
             print("Connessione chiusa.")
 
     def visualizza_piste(self):
-        pass
+        dizionario_colori = {"verde": 0, "blu": 1, "rossa": 2, "nera": 3}
+        piste_ordine = []
+
+        with self.driver.session() as session:
+            risultato = session.run("MATCH (n:SegmentoPista) RETURN DISTINCT n.pista, n.colore;")
+            piste = [record.values() for record in risultato]
+
+            piste_ordinate = sorted(piste, key=lambda x: dizionario_colori.get(x[1], float('inf')))
+
+            for pista in piste_ordinate:
+                valore_pista = dizionario_colori[pista[1]]
+                piste_ordine.append((pista[0], valore_pista))
+                print(pista)
+
+            return piste_ordine
+
 
     def piste_aperte(self):
-        pass
+        with self.driver.session() as session:
+            print("Cerco le piste aperte")
+            risultato = session.run("MATCH (n)-[r:SEGUITE_DA]->(m) WHERE r.stato = 0 RETURN DISTINCT r.pista AS nome_pista;")
+            piste_chiuse = [record["nome_pista"] for record in risultato]
+            print(f"Le piste chiuse sono {piste_chiuse}")
+
+            piste = self.visualizza_piste()
+            piste_totali = [pista[0] for pista in piste]
+
+            piste_aperte = [pista for pista in piste_totali if pista not in piste_chiuse]
+            print(f"Le piste aperte sono {piste_aperte}")
 
     def percorso_breve(self):
         pass
 
     def difficolta_percorso(self):
-        pass
+        print("Difficoltà percorso")
 
 
 if __name__ == "__main__":
     # Esempio di utilizzo della classe NeoManager
-    neo_manager = NeoManager(neo4j_username="neo4j", neo4j_password="Es@meneo4!", neo4j_uri="bolt://0d076a41.databases.neo4j.io:7687/db.scii")
+    neo_manager = NeoManager(neo4j_username="neo4j", neo4j_password="Es@meneo4!", neo4j_uri="bolt+s://0d076a41.databases.neo4j.io:7687")
     
     try:
-        neo_manager.open()
+        neo_manager.apri_connessione()
         # Esegui le operazioni sul database
-        neo_manager.crea_nodi()
-        neo_manager.crea_collegamenti()
+        # neo_manager.crea_nodi()
+        # neo_manager.crea_collegamenti()
         neo_manager.visualizza_piste()
         neo_manager.piste_aperte()
         neo_manager.percorso_breve()
@@ -51,4 +80,4 @@ if __name__ == "__main__":
         print(f"Errore durante l'esecuzione delle operazioni: {e}")
         traceback.print_exc()
     finally:
-        neo_manager.close()
+        neo_manager.chiudi_connessione()
